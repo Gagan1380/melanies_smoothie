e538169d05aff456a242371a4c18ff9c25604b29
Gagan1380 <166055456+Gagan1380@users.noreply.github.com>
2024-04-05 23:20:35 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,9 +30,6 @@ if ingredients_list:
         ingredients_string += fruit_chosen + ' '
 
     st.write ( ingredients_string ) 
-
-    
-
     #st.write(my_insert_stmt)
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
@@ -46,6 +43,7 @@ if ingredients_list:
                
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response.json())
+#st.text(fruityvice_response.json())
+fv_df = st.dataframe(data = fruityvice_response.json(), use_container_width = True)
 
 
