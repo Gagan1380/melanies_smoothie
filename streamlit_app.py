47f3fc6850b92b50a34b87045b1f3479a2a1ac73
Gagan1380 <166055456+Gagan1380@users.noreply.github.com>
2024-04-06 01:19:03 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,8 +4,7 @@ import requests
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie :cup_with_straw:")
 st.write(
-    """Choose the fruits you want in your custom Smoothie!.
-    """)
+    """Choose the fruits you want in your custom Smoothie!. """)
 
 name_on_order = st.text_input ('Name on Smoothie:')
 st.write ('The name of your smoothie will be:',name_on_order)
@@ -24,6 +23,9 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        st.subheader(fruit_chosen + 'Nutrition Information')
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
+        fv_df = st.dataframe(data = fruityvice_response.json(), use_container_width = True)
 
     st.write ( ingredients_string ) 
 
