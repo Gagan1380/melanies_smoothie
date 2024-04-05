4b8f8c7c173b95b713efbccc459414371a2fb966
Gagan1380 <166055456+Gagan1380@users.noreply.github.com>
2024-04-06 01:06:38 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,12 +14,14 @@ cnx = st.connection("snowflake")
 session = cnx.session()
 #session = get_active_session()
 #my_dataframe = session.table("smoothies.public.orders").filter(col("ORDER_FILLED")==0).collect()
+
 my_dataframe = session.table("smoothies.public.fruit_options").select (col('FRUIT_NAME'),col('SEARCH_ON'))
-#st.dataframe(data=my_dataframe, use_container_width=True)
-#st.stop()
+st.dataframe(data=my_dataframe, use_container_width=True)
+st.stop()
 #pd_df = my_dataframe.to_pandas() 
 #st.dataframe(pd_df)
 #st.stop()
+
 ingredients_list = st.multiselect ('Choose upto 6 ingredients:'
             , my_dataframe
             , max_selections=6)
